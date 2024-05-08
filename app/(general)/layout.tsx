ed598c7e2e38550ef272b468ecec8094ba2c22f4
Alfonso Gonzalez <alfonso_gonzalezg@outlook.com>
2024-05-08 12:03:40 -0500

    Layout and nested layout

---
+++ app/(general)/layout.tsx
@@ -0,0 +1,9 @@
+// lrc -> shortcut
+export default function AboutLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <main className="flex flex-col items-center p-24">
+      <span className="text-lg">Hola mundo</span>
+      {children}
+    </main>
+  );
+}
